cb8c577eeabf62cd9dec45f4bac6c5da5a1f8d8a
Neo Yiu <neo.yiu@rakuten.com>
2018-03-17 16:41:47 +0000

    remove empty lines

--- src/com/neo/springdemo/dao/CustomerDAO.java
+++ src/com/neo/springdemo/dao/CustomerDAO.java
@@ -7,12 +7,8 @@ import com.neo.springdemo.entity.Customer;
 public interface CustomerDAO {
 
 	public List<Customer> getCustomers();
-
 	public void saveCustomer(Customer theCustomer);
-
 	public Customer getCustomer(int theId);
-
 	public void deleteCustomer(int theId);
-
 	public List<Customer> searchCustomers(String theSearchName);
 }
